bf7e55c472c3049dbb6cc412b0dd78ae2ff80dc3
littledragon12344 <38128833+littledragon12344@users.noreply.github.com>
2023-08-26 16:10:18 +0800

    Adding DoorAnimation scipt
    - Door rotates depending on which side of the door the player is on

---
+++ Assets/Scripts/Animations/DoorAnimation.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorAnimation : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 1f;
+
+    private int stopAngle;
+    private Vector3 direction;
+    private Vector3 lookDirection;
+    private bool isOpen = false;
+    private bool inRange = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        stopAngle = (int)transform.eulerAngles.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && inRange)
+        {
+            isOpen = !isOpen;
+        }
+
+        if (stopAngle != (int)transform.eulerAngles.y)
+        {
+            transform.Rotate(direction * speed);
+        }
+
+        if (isOpen && lookDirection.z > 0 && stopAngle == 0)
+        {
+            stopAngle = 90;
+            direction = Vector3.up;
+        }
+        else if (isOpen && lookDirection.z < 0 && stopAngle == 0)
+        {
+            stopAngle = 270;
+            direction = Vector3.down;
+        }
+
+        if (!isOpen && stopAngle != 0)
+        {
+            stopAngle = 0;
+            direction = -direction;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            inRange = true;
+
+            lookDirection = transform.position - other.transform.position;
+            lookDirection.Normalize();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            inRange = false;
+            lookDirection = Vector3.zero;
+        }
+    }
+}
