f7f6e8d761e3ab12517b469f7d307f68fa4b89d5
yxnzhnn <142193431+yxnzhnn@users.noreply.github.com>
2023-08-29 17:20:46 +0800

    Minor update to the player GUI - Gia Ngoc

--- Assets/Scripts/UI/PlayerGUIController.cs
+++ Assets/Scripts/UI/PlayerGUIController.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UIElements;
+using UnityEngine.UI;
 using TMPro;
 
 public class PlayerGUIController : MonoBehaviour
@@ -12,6 +12,7 @@ public class PlayerGUIController : MonoBehaviour
     [SerializeField]
     private PlayerStateController playerState;
 
+    [Header("GUI references")]
     [SerializeField]
     private Slider healthBar;
     [SerializeField]
